8a1bb865d3639c4f932a162a450d98a1fe73d9d4
atharv-kanbargi <atharv.kanbargi@somaiya.edu>
2024-09-15 17:10:30 +0530

    end point update

--- src/components/DragDropFiles.jsx
+++ src/components/DragDropFiles.jsx
@@ -25,7 +25,7 @@ const DragDropFiles = () => {
     });
 
     try {
-      const response = await axios.post("https://craft-backend-nine.vercel.app/api/upload", formData, {
+      const response = await axios.post("https://craft-backend-nine.vercel.app/upload", formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
